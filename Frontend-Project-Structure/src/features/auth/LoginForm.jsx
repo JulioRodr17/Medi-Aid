e35a7e3048f69ea9200ecb758fd39fb54447ac8a
Julio Rodriguez Escogido <85967297+JulioRodriguez17@users.noreply.github.com>
2025-10-22 02:48:00 -0600

    Modificacion de estilos a color primerio de elementos primarios.

--- Frontend-Project-Structure/src/features/auth/LoginForm.jsx
+++ Frontend-Project-Structure/src/features/auth/LoginForm.jsx
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './LoginForm.css';
-import logo from '../../../src/assets/images/logoMediAid.jpeg';
 
-import Button from '../../components/ui/button/Button'
-import Input from '../../components/ui/input/Input'
+import Input from '../../components/ui/input/Input';
+import Button from '../../components/ui/button/Button';
+import logo from '../../assets/images/logoMediAid.jpeg';
+
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // 2. Inicializamos el hook de navegación
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
-   {/* Validacion*/}
-    event.preventDefault(); 
-    console.log('Intentando iniciar sesión con:', { email, password });
+    event.preventDefault();
+    console.log('Enviando datos:', { email, password });
+    
+    // TODO: BACKEND
+    // Aquí iría tu lógica de fetch/axios para validar al usuario.
+    // Si la validación es exitosa...
+    
+    // 3. ¡Navegamos al usuario a la página principal!
+    navigate('/'); // Esto te lleva a la HomePage
   };
 
   return (
     <div className="login-form-box">
-      <h2>Iniciar Sesión</h2>
-
       <img src={logo} alt="Logo de Medi-Aid" className="login-logo" />
-      
+      <h2>Iniciar Sesión</h2>
       <form onSubmit={handleSubmit}>
         <Input
           id="email"
@@ -29,23 +36,24 @@ const LoginForm = () => {
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="tu.correo@ejemplo.com"
+          required
         />
-
         <Input
           id="password"
           label="Contraseña"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="••••••••"
+          required
         />
-
-        <Button type="submit" className="login-button">
-          <Link to="/HomePage">Iniciar Sesión</Link>
+        
+        <Button 
+          type="submit" 
+          variant="primary" 
+        >
+          Ingresar
         </Button>
       </form>
-      
       <div className="login-links">
         <Link to="/recuperar-contrasena">¿Olvidaste tu contraseña?</Link>
         <Link to="/registro">¿No tienes cuenta? Regístrate</Link>
@@ -55,4 +63,3 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
-
