e7955fff84da8e76202e480d32fdc5fc888352af
AlmaVT <nayely_vt@outlook.com>
2025-11-10 07:00:43 -0600

    Productivo

--- Frontend-Project-Structure/src/services/httpClient.js
+++ Frontend-Project-Structure/src/services/httpClient.js
@@ -1,6 +1,6 @@
 // utils/httpClient.js
-const API_BASE_URL = 'http://localhost:8080/api';
-
+//const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = '/api';
 const request = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
 
